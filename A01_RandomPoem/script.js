4971d3e0a0698c94cd33f107ce6ee3e924d72d3e
Eyuep06 <80841485+Eyuep06@users.noreply.github.com>
2021-10-14 16:06:00 +0200

    Abgabe1Fertig

--- A01_RandomPoem/script.js
+++ A01_RandomPoem/script.js
@@ -2,15 +2,22 @@ var abgabe2;
 (function (abgabe2) {
     var subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     var praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-    var objekte = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-    console.log(subjekte, praedikate, objekte);
+    var objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    //console.log(subjekte, praedikate, objekte);
     for (var i = subjekte.length; i >= 1; i--) {
-        console.log(i);
-        console.log(getVerse(subjekte, praedikate, objekte));
+        //console.log(i);
+        getVerse(subjekte, praedikate, objekte);
     }
     function getVerse(_subjekte, _praedikate, _objekte) {
-        var verse = [""];
-        console.log("Alohomora");
+        var verse = "";
+        var ergebnisSubjekte;
+        var ergebnisPraedikate;
+        var ergebnisObjekte;
+        ergebnisSubjekte = Math.floor(Math.random() * subjekte.length);
+        ergebnisPraedikate = Math.floor(Math.random() * praedikate.length);
+        ergebnisObjekte = Math.floor(Math.random() * objekte.length);
+        verse = verse + _subjekte.splice(ergebnisSubjekte, 1) + " " + _praedikate.splice(ergebnisPraedikate, 1) + " " + _objekte.splice(ergebnisObjekte, 1);
+        console.log(verse);
     }
 })(abgabe2 || (abgabe2 = {}));
 //# sourceMappingURL=script.js.map
--- A01_RandomPoem/script.ts
+++ A01_RandomPoem/script.ts
@@ -2,25 +2,47 @@ namespace abgabe2 {
 
     let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-    let objekte: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objekte: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
-    console.log(subjekte, praedikate, objekte);
-    
+    //console.log(subjekte, praedikate, objekte);
+
 
 
     for (let i: number = subjekte.length; i >= 1; i--) {
-        console.log(i);
-        console.log(getVerse(subjekte, praedikate, objekte));
+        //console.log(i);
         
-    }
-
-    function getVerse (_subjekte: string[], _praedikate: string[], _objekte: string[]): void {
-        let verse:  string[] = [""];
-        console.log("Alohomora");
-
-    }
-
+        getVerse(subjekte, praedikate, objekte);
     
+
+    }
+
+
+
+    function getVerse(_subjekte: string[], _praedikate: string[], _objekte: string[]): void {
+        
+        
+        let verse: string = "";        
+
+        let ergebnisSubjekte: number;
+        let ergebnisPraedikate: number;
+        let ergebnisObjekte: number;
+        
+
+        ergebnisSubjekte = Math.floor (Math.random() * subjekte.length);
+        ergebnisPraedikate = Math.floor (Math.random() * praedikate.length);
+        ergebnisObjekte = Math.floor (Math.random() * objekte.length);
+        
+        verse = verse +  _subjekte.splice(ergebnisSubjekte, 1) + " " + _praedikate.splice(ergebnisPraedikate, 1) + " " + _objekte.splice(ergebnisObjekte, 1);
+        
+        console.log(verse);
+        
+       
+        
+        
+
+    }
+
+
 
 
 }
