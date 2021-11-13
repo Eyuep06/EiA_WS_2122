73ffa9ba69003d0ce3d8b19e1279d9f14f9d24ff
Eyuep06 <80841485+Eyuep06@users.noreply.github.com>
2021-11-13 13:19:07 +0100

    zwischenspeichern

--- L03_SequenzmemorySettings/script.js
+++ L03_SequenzmemorySettings/script.js
@@ -4,36 +4,45 @@ var L03Sequenzmemory;
     document.querySelector("button")?.addEventListener("click", startGame);
     let benutzereingabeArray = [];
     let benutzereingabe;
+    let shuffledArray = [];
     function startGame() {
         document.getElementById("fsSpecial")?.classList.add("isHidden");
         let eingabefeldWert = document.getElementById("eingabefeld");
         benutzereingabe = eingabefeldWert.value;
         benutzereingabeArray = benutzereingabe.split("");
+        shuffledArray = benutzereingabe.split("");
         shuffle(benutzereingabeArray);
         createfield();
     }
     function shuffle(_array) {
-        let currentIndex = benutzereingabeArray.length, randomIndex;
+        let currentIndex = shuffledArray.length, randomIndex;
         while (currentIndex != 0) {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
-            [benutzereingabeArray[currentIndex], benutzereingabeArray[randomIndex]] = [benutzereingabeArray[randomIndex], benutzereingabeArray[currentIndex]];
+            [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [shuffledArray[randomIndex], shuffledArray[currentIndex]];
         }
         console.log(benutzereingabeArray);
         console.log(benutzereingabe);
+        console.log(shuffledArray);
         return _array;
     }
     function createfield() {
-        let letters = benutzereingabeArray.length - 1;
+        let letters = shuffledArray.length - 1;
         while (letters >= 0) {
             let spielfeld = document.getElementById("spielfeld");
             let karte = document.createElement("span");
             spielfeld.appendChild(karte);
+            karte.addEventListener("click", check);
             let buchstabe = document.createElement("p");
             karte.appendChild(buchstabe);
-            buchstabe.innerHTML = benutzereingabeArray[letters];
+            buchstabe.innerHTML = shuffledArray[letters];
             letters--;
+            setTimeout(function () { buchstabe.classList.add("isHidden"); }, 5000);
         }
+    }
+    function check(_event) {
+        console.log("ich wurde geklickt");
+        // is clicked p value same as value 0 of correct sequenz array
     }
 })(L03Sequenzmemory || (L03Sequenzmemory = {}));
 //# sourceMappingURL=script.js.map
--- L03_SequenzmemorySettings/script.ts
+++ L03_SequenzmemorySettings/script.ts
@@ -2,6 +2,7 @@ namespace L03Sequenzmemory {
     document.querySelector("button")?.addEventListener("click", startGame);
     let benutzereingabeArray: string[] = [];
     let benutzereingabe: string;
+    let shuffledArray: string[] = [];
 
 
 
@@ -11,6 +12,8 @@ namespace L03Sequenzmemory {
         let eingabefeldWert: HTMLInputElement = <HTMLInputElement>document.getElementById("eingabefeld");
         benutzereingabe = eingabefeldWert.value;
         benutzereingabeArray = benutzereingabe.split("");
+        shuffledArray = benutzereingabe.split("");
+
 
         shuffle(benutzereingabeArray);
         createfield();
@@ -18,36 +21,48 @@ namespace L03Sequenzmemory {
     }
 
     function shuffle(_array: string[]): string[] {
-        let currentIndex: number = benutzereingabeArray.length, randomIndex: number;
+        let currentIndex: number = shuffledArray.length, randomIndex: number;
         while (currentIndex != 0) {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
-            [benutzereingabeArray[currentIndex], benutzereingabeArray[randomIndex]] = [benutzereingabeArray[randomIndex], benutzereingabeArray[currentIndex]];
+            [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [shuffledArray[randomIndex], shuffledArray[currentIndex]];
 
         }
 
         console.log(benutzereingabeArray);
         console.log(benutzereingabe);
+        console.log(shuffledArray);
+        
 
         return _array;
     }
 
     function createfield(): void {
-        let letters: number = benutzereingabeArray.length - 1;
+        let letters: number = shuffledArray.length - 1;
         while (letters >= 0) {
 
             let spielfeld: HTMLElement = <HTMLSpanElement>document.getElementById("spielfeld");
             let karte: HTMLSpanElement = document.createElement("span");
             spielfeld.appendChild(karte);
+            karte.addEventListener("click", check);
+
             let buchstabe: HTMLParagraphElement = <HTMLParagraphElement>document.createElement("p");
             karte.appendChild(buchstabe);
-            buchstabe.innerHTML = benutzereingabeArray[letters];
+            buchstabe.innerHTML = shuffledArray[letters];
             letters--;
-
+            setTimeout(function (): void { buchstabe.classList.add("isHidden"); }, 5000);
         }
+
     }
 
-
-
+    function check(_event: Event): void {
+        console.log("ich wurde geklickt");
+        // is clicked p value same as value 0 of correct sequenz array
+        
+    }
 }
+
+
+
+
 
