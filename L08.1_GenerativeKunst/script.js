b03cb8224ce843888224a8370178dc6efac54bf9
Eyuep06 <80841485+Eyuep06@users.noreply.github.com>
2021-11-17 16:53:49 +0100

    L08_1_angefangen

--- L08.1_GenerativeKunst/script.js
+++ L08.1_GenerativeKunst/script.js
@@ -1,19 +1,25 @@
 "use strict";
 var GenerativeKunst;
 (function (GenerativeKunst) {
-    console.log("hallo");
-    let canvas = document.querySelector("canvas");
-    let crc2 = canvas.getContext("2d");
+    let crc2;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
-        let x = Math.random() * 800;
-        let y = Math.random() * 600;
-        drawArt(x, y);
+        let canvas = document.querySelector("canvas");
+        crc2 = canvas.getContext("2d");
+        for (let i = 0; i < 99; i++) {
+            let x = Math.random() * 600;
+            let y = Math.random() * 450;
+            drawArt(x, y);
+        }
     }
     function drawArt(_x, _y) {
+        // alle _x und _y bekommen die gleiche Zahl, wie kriegt jeder eine eigene random zahl
         crc2.beginPath();
-        crc2.moveTo(_x, _y - 30);
-        crc2.lineTo(_x - 60, _y);
+        crc2.moveTo(_x, _y);
+        crc2.lineTo(_x, _y);
+        crc2.lineTo(_x, _y);
+        crc2.lineTo(_x, _y);
+        crc2.lineTo(_x, _y);
         crc2.lineTo(_x, _y);
         crc2.closePath();
         crc2.fillStyle = "#FF0000"; //randomColor
--- L08.1_GenerativeKunst/script.ts
+++ L08.1_GenerativeKunst/script.ts
@@ -1,21 +1,27 @@
 namespace GenerativeKunst {
-    console.log("hallo");
-    let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas");
-    let crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d");
+    let crc2: CanvasRenderingContext2D;
     window.addEventListener ("load", handleLoad);
     
     
 
     function handleLoad(_event: Event): void {
-        let x: number = Math.random() * 800;
-        let y: number = Math.random() * 600;
+        let canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+        crc2 = canvas.getContext("2d")!;
+        for (let i: number = 0; i < 99; i++) {
+        let x: number = Math.random() * 600;
+        let y: number = Math.random() * 450;
         drawArt(x, y);
+    }
     }
 
     function drawArt(_x: number, _y: number): void {
+    // alle _x und _y bekommen die gleiche Zahl, wie kriegt jeder eine eigene random zahl
     crc2.beginPath();
-    crc2.moveTo(_x, _y - 30);
-    crc2.lineTo(_x - 60, _y);
+    crc2.moveTo(_x, _y);
+    crc2.lineTo(_x, _y);
+    crc2.lineTo(_x, _y);
+    crc2.lineTo(_x, _y);
+    crc2.lineTo(_x, _y);
     crc2.lineTo(_x, _y);
     crc2.closePath();
     
