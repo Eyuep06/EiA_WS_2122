4019f417779626c78a1c1373fb258490930df877
Eyuep06 <80841485+Eyuep06@users.noreply.github.com>
2021-11-18 12:19:33 +0100

    zwischenspeichern

--- L08.1_GenerativeKunst/script.js
+++ L08.1_GenerativeKunst/script.js
@@ -6,25 +6,38 @@ var GenerativeKunst;
     function handleLoad(_event) {
         let canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
-        for (let i = 0; i < 99; i++) {
-            let x = Math.random() * 600;
-            let y = Math.random() * 450;
-            drawArt(x, y);
+        for (let i = 0; i < 29; i++) {
+            // let x: number = Math.random() * 600;
+            // let y: number = Math.random() * 450;
+            drawAngular();
+            drawCircle();
         }
     }
-    function drawArt(_x, _y) {
+    function drawAngular() {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
         // alle _x und _y bekommen die gleiche Zahl, wie kriegt jeder eine eigene random zahl
         crc2.beginPath();
-        crc2.moveTo(_x, _y);
-        crc2.lineTo(_x, _y);
-        crc2.lineTo(_x, _y);
-        crc2.lineTo(_x, _y);
-        crc2.lineTo(_x, _y);
-        crc2.lineTo(_x, _y);
-        crc2.closePath();
-        crc2.fillStyle = "#FF0000"; //randomColor
-        crc2.strokeStyle = "blue"; //randomColor
-        crc2.lineWidth = 2; //randomNum
+        crc2.moveTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        //crc2.closePath();
+        crc2.fillStyle = "#" + randomColor;
+        crc2.strokeStyle = "#" + randomColor;
+        crc2.lineWidth = 2;
+        crc2.stroke();
+        crc2.fill();
+    }
+    function drawCircle() {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        crc2.beginPath();
+        crc2.arc(Math.random() * 600, Math.random() * 450, 50, 0, 2 * Math.PI);
+        crc2.fillStyle = "#" + randomColor;
+        crc2.strokeStyle = "#" + randomColor;
+        crc2.lineWidth = 2;
         crc2.stroke();
         crc2.fill();
     }
--- L08.1_GenerativeKunst/script.ts
+++ L08.1_GenerativeKunst/script.ts
@@ -1,38 +1,57 @@
 namespace GenerativeKunst {
     let crc2: CanvasRenderingContext2D;
-    window.addEventListener ("load", handleLoad);
-    
-    
+    window.addEventListener("load", handleLoad);
+
+
 
     function handleLoad(_event: Event): void {
+
         let canvas: HTMLCanvasElement = document.querySelector("canvas")!;
         crc2 = canvas.getContext("2d")!;
-        for (let i: number = 0; i < 99; i++) {
-        let x: number = Math.random() * 600;
-        let y: number = Math.random() * 450;
-        drawArt(x, y);
-    }
-    }
-
-    function drawArt(_x: number, _y: number): void {
-    // alle _x und _y bekommen die gleiche Zahl, wie kriegt jeder eine eigene random zahl
-    crc2.beginPath();
-    crc2.moveTo(_x, _y);
-    crc2.lineTo(_x, _y);
-    crc2.lineTo(_x, _y);
-    crc2.lineTo(_x, _y);
-    crc2.lineTo(_x, _y);
-    crc2.lineTo(_x, _y);
-    crc2.closePath();
-    
-    crc2.fillStyle = "#FF0000"; //randomColor
-    crc2.strokeStyle = "blue"; //randomColor
-    crc2.lineWidth = 2 ; //randomNum
-    crc2.stroke();
-    crc2.fill();
+        for (let i: number = 0; i < 29; i++) {
+            // let x: number = Math.random() * 600;
+            // let y: number = Math.random() * 450;
+            drawAngular();
+            drawCircle();
+        }
     }
 
+    function drawAngular(): void {
+        let randomColor: string = Math.floor(Math.random() * 16777215).toString(16);
+        // alle _x und _y bekommen die gleiche Zahl, wie kriegt jeder eine eigene random zahl
+        crc2.beginPath();
+        crc2.moveTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        crc2.lineTo(Math.random() * 600, Math.random() * 450);
+        //crc2.closePath();
 
+        crc2.fillStyle = "#" + randomColor;
+        crc2.strokeStyle = "#" + randomColor;
+        crc2.lineWidth = 2;
+        crc2.stroke();
+        crc2.fill();
+    }
+
+
+    function drawCircle(): void {
+        let randomColor: string = Math.floor(Math.random() * 16777215).toString(16);
+        crc2.beginPath();
+        crc2.arc(Math.random() * 600, Math.random() * 450, 50, 0, 2 * Math.PI);
+        crc2.fillStyle = "#" + randomColor;
+        crc2.strokeStyle = "#" + randomColor;
+        crc2.lineWidth = 2;
+        crc2.stroke();
+        crc2.fill();
+
+    }
+
+
+
+   
 
 
 }
